eb78626b1618f7477931ad7e015f67202cd1aa68
Miraslava Bulhakava <mirabulhakava@gmail.com>
2021-06-23 16:33:57 +0300

    added error localization

--- web/src/main/java/com/epam/esm/config/SpringConfig.java
+++ web/src/main/java/com/epam/esm/config/SpringConfig.java
@@ -1,7 +1,9 @@
 package com.epam.esm.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -23,4 +25,11 @@ public class SpringConfig implements WebMvcConfigurer {
         converters.add(jacksonConverter);
     }
 
+    @Bean
+    public ResourceBundleMessageSource getResourceBundleMessageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.addBasenames("/errorMessage");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
 }
