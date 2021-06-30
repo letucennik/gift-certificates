a1c69da5d01041273570d699a68b839e56056403
Miraslava Bulhakava <mirabulhakava@gmail.com>
2021-06-30 15:04:30 +0300

    changed Tag and GiftCertificate

--- repository/src/main/java/com/epam/esm/entity/GiftCertificate.java
+++ repository/src/main/java/com/epam/esm/entity/GiftCertificate.java
@@ -5,22 +5,41 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@EntityListeners(AuditListener.class)
+@Table(name = "gift_certificate")
 public class GiftCertificate implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
     private long id;
+
+    @Column
     private String name;
+
+    @Column
     private String description;
+
+    @Column
     private BigDecimal price;
+
+    @Column(name="create_date")
     private LocalDateTime createDate;
+
+    @Column(name="last_update_date")
     private LocalDateTime lastUpdateDate;
+
+    @Column
     private int duration;
 
     public GiftCertificate(long id, String name, String description, BigDecimal price, int duration) {
