13a5d3b29f88981ffc41ad3542d95c01ab9bffb0
Miraslava Bulhakava <mirabulhakava@gmail.com>
2021-07-13 21:30:33 +0300

    minor refactoring

--- repository/src/main/java/com/epam/esm/repository/impl/TagRepositoryImpl.java
+++ repository/src/main/java/com/epam/esm/repository/impl/TagRepositoryImpl.java
@@ -1,8 +1,8 @@
 package com.epam.esm.repository.impl;
 
+import com.epam.esm.repository.TagRepository;
 import com.epam.esm.repository.entity.Tag;
 import com.epam.esm.repository.exception.DAOException;
-import com.epam.esm.repository.TagRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -15,6 +15,17 @@ import java.util.Optional;
 
 @Repository
 public class TagRepositoryImpl implements TagRepository {
+
+    private static final String GET_MOST_WIDELY_USED_TAG = "SELECT tag_id AS id, tag_name AS name FROM " +
+            " (SELECT tag.id AS tag_id, tag.name AS tag_name, COUNT(tag_id) AS tag_count FROM tag " +
+            " JOIN m2m_certificates_tags ON tag.id = m2m_certificates_tags.tag_id " +
+            " JOIN gift_certificate ON m2m_certificates_tags.gift_certificate_id = gift_certificate.id " +
+            " JOIN order_certificates ON gift_certificate.id = order_certificates.certificate_id " +
+            " JOIN orders ON order_certificates.order_id = orders.id " +
+            " JOIN (SELECT SUM(cost) AS orders_cost, user_id AS ui FROM orders GROUP BY user_id)" +
+            " AS a ON orders.user_id = a.ui WHERE orders_cost = " +
+            " (SELECT SUM(cost) AS orders_cost FROM orders GROUP BY user_id ORDER BY orders_cost DESC LIMIT 1)" +
+            " GROUP BY tag_id ORDER BY tag_count DESC LIMIT 1) AS b";
 
     @PersistenceContext
     private EntityManager entityManager;
@@ -56,16 +67,7 @@ public class TagRepositoryImpl implements TagRepository {
     @Override
     public Tag getMostWidelyUsedTag() {
         return (Tag) entityManager
-                .createNativeQuery("SELECT tag_id AS id, tag_name AS name FROM " +
-                        " (SELECT tag.id AS tag_id, tag.name AS tag_name, COUNT(tag_id) AS tag_count FROM tag " +
-                        " JOIN m2m_certificates_tags ON tag.id = m2m_certificates_tags.tag_id " +
-                        " JOIN gift_certificate ON m2m_certificates_tags.gift_certificate_id = gift_certificate.id " +
-                        " JOIN order_certificates ON gift_certificate.id = order_certificates.certificate_id " +
-                        " JOIN orders ON order_certificates.order_id = orders.id " +
-                        " JOIN (SELECT SUM(cost) AS orders_cost, user_id AS ui FROM orders GROUP BY user_id)" +
-                        " AS a ON orders.user_id = a.ui WHERE orders_cost = " +
-                        " (SELECT SUM(cost) AS orders_cost FROM orders GROUP BY user_id ORDER BY orders_cost DESC LIMIT 1)" +
-                        " GROUP BY tag_id ORDER BY tag_count DESC LIMIT 1) AS b", Tag.class)
+                .createNativeQuery(GET_MOST_WIDELY_USED_TAG, Tag.class)
                 .getSingleResult();
     }
 
