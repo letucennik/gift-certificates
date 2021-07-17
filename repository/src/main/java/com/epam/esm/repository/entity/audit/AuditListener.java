d1cdac43a77454d0a7d9afa16c749bf60b0ef194
Miraslava Bulhakava <mirabulhakava@gmail.com>
2021-07-17 21:06:50 +0300

    added OrderRepositoryImpl

---
+++ repository/src/main/java/com/epam/esm/repository/entity/audit/AuditListener.java
@@ -0,0 +1,57 @@
+package com.epam.esm.repository.entity.audit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.persistence.PostPersist;
+import javax.persistence.PostRemove;
+import javax.persistence.PostUpdate;
+import javax.persistence.PrePersist;
+import javax.persistence.PreRemove;
+import javax.persistence.PreUpdate;
+
+public class AuditListener {
+
+    private static final String PRE_INSERT = "Pre insert: ";
+    private static final String POST_INSERT = "Post insert: ";
+    private static final String PRE_UPDATE = "Pre update: ";
+    private static final String POST_UPDATE = "Post update: ";
+    private static final String PRE_REMOVE = "Pre remove: ";
+    private static final String POST_REMOVE = "Post remove: ";
+
+    private final Logger logger = LoggerFactory.getLogger(AuditListener.class);
+
+    @PrePersist
+    private void prePersistAudit(Object object) {
+        logger.info(PRE_INSERT + object);
+    }
+
+    @PostPersist
+    public void onPostPersist(Object object) {
+        logger.info(POST_INSERT + object);
+    }
+
+    @PreUpdate
+    public void onPreUpdate(Object object) {
+        logger.info(PRE_UPDATE + object);
+
+    }
+
+    @PostUpdate
+    public void onPostUpdate(Object object) {
+        logger.info(POST_UPDATE + object);
+
+    }
+
+    @PreRemove
+    public void onPreRemove(Object object) {
+        logger.info(PRE_REMOVE + object);
+
+    }
+
+    @PostRemove
+    public void onPostRemove(Object object) {
+        logger.info(POST_REMOVE + object);
+    }
+
+}
---
+++ repository/src/main/java/com/epam/esm/repository/impl/OrderRepositoryImpl.java
@@ -0,0 +1,74 @@
+package com.epam.esm.repository.impl;
+
+import com.epam.esm.repository.OrderRepository;
+import com.epam.esm.repository.entity.Order;
+import com.epam.esm.repository.entity.User;
+import com.epam.esm.repository.exception.DAOException;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceException;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class OrderRepositoryImpl implements OrderRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public Order create(Order order) {
+        try {
+            entityManager.persist(order);
+        } catch (PersistenceException e) {
+            throw new DAOException(e);
+        }
+        return order;
+    }
+
+    @Override
+    public Optional<Order> read(long id) {
+        return Optional.ofNullable(entityManager.find(Order.class, id));
+    }
+
+    @Override
+    public Optional<Order> findByUserId(long userId, long id) {
+        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Order> criteria = builder.createQuery(Order.class);
+        Root<Order> root = criteria.from(Order.class);
+        Predicate userPredicate = builder.equal(root.get("user").get("id"), userId);
+        Predicate orderPredicate = builder.equal(root.get("id"), id);
+        criteria.where(userPredicate, orderPredicate);
+        return entityManager.createQuery(criteria).getResultStream().findAny();
+    }
+
+    @Override
+    public List<Order> getUserOrders(long userId, Pageable pageable) {
+        return entityManager.createQuery(buildCriteriaQuery(userId))
+                .setMaxResults(pageable.getPageSize())
+                .setFirstResult((int) pageable.getOffset())
+                .getResultList();
+    }
+
+    private CriteriaQuery<Order> buildCriteriaQuery(long userId) {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Order> criteriaQuery = criteriaBuilder.createQuery(Order.class);
+        Predicate joinIdPredicate = joinIdPredicate(criteriaBuilder, criteriaQuery, userId);
+        criteriaQuery.where(joinIdPredicate);
+        return criteriaQuery;
+    }
+
+    private Predicate joinIdPredicate(CriteriaBuilder criteriaBuilder, CriteriaQuery<Order> criteriaQuery, long userId) {
+        Root<Order> root = criteriaQuery.from(Order.class);
+        Join<User, Order> userJoin = root.join("user");
+        return criteriaBuilder.equal(userJoin.get("id"), userId);
+    }
+}
